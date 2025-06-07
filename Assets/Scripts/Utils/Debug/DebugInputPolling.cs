82a0679da1bf4303fe215079959cfa86c2843697
Kevin Tan <67160289+kevinn-t@users.noreply.github.com>
2025-06-07 15:02:24 -0700

    Loot items (#6)

---
+++ Assets/Scripts/Utils/Debug/DebugInputPolling.cs
@@ -0,0 +1,21 @@
+using CMPM.Level;
+using UnityEngine;
+
+namespace CMPM.Utils.Debug
+{
+    public class DebugInputPolling : MonoBehaviour
+    {
+        public LootController lc;
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Backspace))
+            {
+                // lc._type = LootController.LootType.SPELL;
+                lc._type = LootController.LootType.RELIC;
+                var _new = Instantiate(lc);
+                _new.transform.position = new Vector3(Random.Range(-5,6), Random.Range(-5,6), -1);
+            }
+        }
+    }
+}
