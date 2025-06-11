3b7c3a3bed4572c836f9904f963a533efd027c83
Thom Mott <thommott@proton.me>
2025-06-10 18:43:07 -0700

    room generation moved to main

--- Assets/Scripts/Movement/EnemyController.cs
+++ Assets/Scripts/Movement/EnemyController.cs
@@ -9,8 +9,7 @@ using UnityEngine.Serialization;
 
 
 namespace CMPM.Movement {
-    public class EnemyController : Entity
-    {
+    public class EnemyController : Entity {
         #region Publics
         public Transform target;
         [FormerlySerializedAs("healthui")] public HealthBar healthUI;
@@ -30,98 +29,74 @@ namespace CMPM.Movement {
         public BehaviourTree Behaviour;
         public AudioSource audioSource;
 
-        Coroutine sfxCoroutine;
+        Coroutine _sfxCoroutine;
 
         float volPitch = 1f;
-
         #endregion
 
-        void Start()
-        {
-            target = GameManager.Instance.Player.transform;
+        void Start() {
+            target     =  GameManager.Instance.Player.transform;
             HP.OnDeath += Die;
             healthUI.SetHealth(HP);
 
-            unit = GetComponent<Unit>();
+            unit  = GetComponent<Unit>();
             _pips = new List<GameObject>();
 
             audioSource = GetComponent<AudioSource>();
-            if (audioSource == null)
-            {
+            if (!audioSource) {
                 audioSource = gameObject.AddComponent<AudioSource>();
             }
+
             audioSource.spatialBlend = 1f;
-            audioSource.minDistance = 5f;
-            audioSource.maxDistance = 40f;
-            audioSource.rolloffMode = AudioRolloffMode.Linear;
+            audioSource.minDistance  = 5f;
+            audioSource.maxDistance  = 40f;
+            audioSource.rolloffMode  = AudioRolloffMode.Linear;
 
-            sfxCoroutine = StartCoroutine(PlayEnemySFX());
+            _sfxCoroutine = StartCoroutine(PlayEnemySFX());
         }
 
-        private SoundTypeEnemies? GetSoundTypeFromName(string name)
-        {
-            switch (name.ToLower())
-            {
-                case "zombie": return SoundTypeEnemies.ZOMBIE;
-                case "skeleton": return SoundTypeEnemies.SKELETON;
-                case "warlock": return SoundTypeEnemies.WARLOCK;
-                case "dryad": return SoundTypeEnemies.DRYAD;
-                case "goat": return SoundTypeEnemies.GOAT;
-                default: return null;
-            }
+        SoundTypeEnemies? GetSoundTypeFromName(string name) {
+            return name.ToLower() switch {
+                "zombie"   => SoundTypeEnemies.ZOMBIE,
+                "skeleton" => SoundTypeEnemies.SKELETON,
+                "warlock"  => SoundTypeEnemies.WARLOCK,
+                "dryad"    => SoundTypeEnemies.DRYAD,
+                "goat"     => SoundTypeEnemies.GOAT,
+                _          => null
+            };
         }
 
-        System.Collections.IEnumerator PlayEnemySFX()
-        {
-            while (!dead)
-            {
+        System.Collections.IEnumerator PlayEnemySFX() {
+            while (!dead) {
                 yield return new WaitForSeconds(Random.Range(7f, 32f));
-                var soundType = GetSoundTypeFromName(enemyName);
-                if (soundType.HasValue)
-                {
-                    audioSource.pitch = volPitch;
-                    switch (soundType.Value)
-                    {
-                        case SoundTypeEnemies.ZOMBIE:
-                            volPitch = Random.Range(0.5f, 1.5f);
-                            break;
-                        case SoundTypeEnemies.SKELETON:
-                            volPitch = Random.Range(2.0f, 3.0f);
-                            break;
-                        case SoundTypeEnemies.WARLOCK:
-                            volPitch = Random.Range(0.5f, 1.5f);
-                            break;
-                        case SoundTypeEnemies.DRYAD:
-                            volPitch = Random.Range(0.5f, 1.5f);
-                            break;
-                        case SoundTypeEnemies.GOAT:
-                            volPitch = Random.Range(0.5f, 2.5f);
-                            break;
-                        default:
-                            volPitch = Random.Range(0.1f, 3.0f);
-                            break;
-                    }
-                    audioSource.PlayOneShot(EnemiesSoundManager.GetEnemyClip(soundType.Value));
-                }
+                SoundTypeEnemies? soundType = GetSoundTypeFromName(enemyName);
+                if (!soundType.HasValue) continue;
+                audioSource.pitch = volPitch;
+                volPitch = soundType.Value switch {
+                    SoundTypeEnemies.ZOMBIE   => Random.Range(0.5f, 1.5f),
+                    SoundTypeEnemies.SKELETON => Random.Range(2.0f, 3.0f),
+                    SoundTypeEnemies.WARLOCK  => Random.Range(0.5f, 1.5f),
+                    SoundTypeEnemies.DRYAD    => Random.Range(0.5f, 1.5f),
+                    SoundTypeEnemies.GOAT     => Random.Range(0.5f, 2.5f),
+                    _                         => Random.Range(0.1f, 3.0f)
+                };
+
+                audioSource.PlayOneShot(EnemiesSoundManager.GetEnemyClip(soundType.Value));
             }
         }
 
-        void Update()
-        {
+        void Update() {
             if (GameManager.Instance.State != GameManager.GameState.INWAVE)
                 Destroy(gameObject);
-            else
-            {
+            else {
                 int str = GetEffect("strength");
-                while (str > _pips.Count)
-                {
+                while (str > _pips.Count) {
                     GameObject newPip = Instantiate(strengthPip, transform);
                     newPip.transform.localPosition = new Vector3(-0.4f + _pips.Count * 0.125f, -0.55f, 0);
                     _pips.Add(newPip);
                 }
 
-                while (_pips.Count > str)
-                {
+                while (_pips.Count > str) {
                     GameObject pip = _pips[^1];
                     _pips.RemoveAt(_pips.Count - 1);
                     Destroy(pip);
@@ -132,19 +107,16 @@ namespace CMPM.Movement {
             }
         }
 
-        public void AddAction(EnemyActionTypes actionType, EnemyAction action)
-        {
-            action.Enemy = this;
+        public void AddAction(EnemyActionTypes actionType, EnemyAction action) {
+            action.Enemy         = this;
             _actions[actionType] = action;
         }
 
-        public EnemyAction GetAction(EnemyActionTypes actionType)
-        {
+        public EnemyAction GetAction(EnemyActionTypes actionType) {
             return _actions.GetValueOrDefault(actionType, null);
         }
 
-        public void AddEffect(string effectName, int stacks)
-        {
+        public void AddEffect(string effectName, int stacks) {
             _effects ??= new Dictionary<string, int>();
             _effects.TryAdd(effectName, 0);
 
@@ -152,18 +124,15 @@ namespace CMPM.Movement {
             if (_effects[effectName] > 10) _effects[effectName] = 10;
         }
 
-        public int GetEffect(string effectName)
-        {
+        public int GetEffect(string effectName) {
             return _effects?.GetValueOrDefault(effectName, 0) ?? 0;
         }
 
-        void Die()
-        {
+        void Die() {
             if (dead) return;
             dead = true;
             EventBus.Instance.DoEnemyDeath(this);
             GameManager.Instance.RemoveEnemy(gameObject);
         }
-        
     }
 }
